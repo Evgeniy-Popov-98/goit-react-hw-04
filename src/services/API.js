caa091e4e430c3b7b977b9f05fc79a71c665b14b
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-03-29 22:46:23 +0200

    add-Api

--- src/services/API.js
+++ src/services/API.js
@@ -3,15 +3,13 @@ import axios from "axios";
 const ACCESS_KEY = "uRbjthHwB56Dd64vCktCP15eYSWhxayYeNHDzHA5aBQ";
 
 export async function getImages(search) {
-  const BASE_URL = "https://api.unsplash.com/";
-  const END_POINT = `${search}/photos/`;
-  const url = `${BASE_URL}${END_POINT}`;
+  const url = "https://api.unsplash.com/search/photos";
   const params = {
-    headers: {
-      Authorization: `${ACCESS_KEY}`,
-    },
+    client_id: `${ACCESS_KEY}`,
+    page: 1,
+    query: `${search}`,
   };
-  const res = await axios.get(url, params);
+  const res = await axios.get(url, { params });
   if (res.data.length === 0) {
     throw new Error("Error");
   } else {
