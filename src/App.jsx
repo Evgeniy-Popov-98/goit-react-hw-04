9ae7e4a6d7e6fcd135b6c4b32c20c60d0fad3036
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-03-30 16:39:37 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ImageGallery from "./components/ImageGallery/ImageGallery";
 import Loader from "./components/Loader/Loader";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
-// import ImageModal from "./components/ImageModal/ImageModal";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
   const [cardArr, setCardArr] = useState([]);
@@ -16,6 +16,8 @@ function App() {
   const [valueInput, setValueOnpit] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
   const [showLoreMore, setShowLoreMore] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalValueImg, setModalValueImg] = useState(null);
 
   useEffect(() => {
     async function dataImages() {
@@ -50,13 +52,29 @@ function App() {
     setPageNumber(newpage);
   };
 
+  const openModal = (event) => {
+    setModalIsOpen(event.bool);
+    setModalValueImg(event);
+  };
+
   return (
     <>
       <SearchBar onSubmit={onSubmit} />
       {loader && <Loader />}
-      {error ? <ErrorMessage /> : <ImageGallery cardImages={cardArr} />}
+      {error ? (
+        <ErrorMessage />
+      ) : (
+        <ImageGallery cardImages={cardArr} openModal={openModal} />
+      )}
       {showLoreMore && (
         <LoadMoreBtn onClick={onClick} pageNumber={pageNumber} />
+      )}
+      {modalIsOpen && (
+        <ImageModal
+          modalIsOpen={modalIsOpen}
+          openModal={openModal}
+          cardImages={modalValueImg}
+        />
       )}
     </>
   );
--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,17 @@
 import clsx from "clsx";
 import style from "./ImageCard.module.css";
 
-const ImageCard = ({ dataImages }) => {
+const ImageCard = ({ dataImages, openModal }) => {
+  const handleClick = () => {
+    const object = {
+      bool: true,
+      src: dataImages.regular,
+      alt: dataImages.description,
+    };
+    openModal(object);
+  };
   return (
-    <li className={clsx(style.galleryItem)}>
+    <li className={clsx(style.galleryItem)} onClick={handleClick}>
       <div>
         <img
           className={clsx(style.galleryImage)}
